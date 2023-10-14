c26f7f16bd4fab1607bbb27c3984e23a6450e950
MohammadMeharaj <147807340+MohammadMeharaj@users.noreply.github.com>
2023-10-14 12:55:14 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class fibonacci {
+  public static void main(String[] args) {
+
+    int n = 10, a = 0, b = 1;
+    System.out.println("Fibonacci Series till " + n + " terms:");
+
+    for (int i = 1; i <= n; ++i) {
+      System.out.print(a + ", ");
+
+      // compute the next term
+      int c = a + b;
+      a = b;
+      b = c;
+    }
+  }
+}
